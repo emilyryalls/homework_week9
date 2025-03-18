046f2f3f15ee1ed4b270a3ce188b34d3e438839c
Ivon Martinez <ivon.mrtnz@gmail.com>
2025-03-18 00:03:40 +0000

    Created procedure to ReturnBook

---
+++ procedureReturnBook.sql
@@ -0,0 +1,28 @@
+ use library_group_3;
+ 
+ DELIMITER //
+ 
+create procedure ReturnBook(
+in UserFirstName varchar(100), in BorrowedBook varchar(255), in Email varchar(255))
+begin
+	update loan as l
+    join user as u on u.UserID = l.UserID
+    join copy as c on l.CopyID = c.CopyID
+    join book as b on b.BookID = c. BookID
+    set ReturnDate = curdate()
+    where u.FirstName = UserFirstName
+    and b.Title = BorrowedBook
+	and u.UserEmail = Email
+    and l.ReturnDate is null;
+
+END//
+
+DELIMITER ;
+
+drop procedure ReturnBook;
+
+call ReturnBook('Tim', 'Pride and Prejudice', 'lerqwc@sd.com');
+
+select *
+from loan;
+ 
